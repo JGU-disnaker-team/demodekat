695b932b92f0765cea3d1ac3fcbbcabc3b35b734
Zamir Mustafid <aryobimo354@gmail.com>
2025-02-22 08:27:49 +0700

    Add fillable attributes and user relationship to WorkerProof model

--- app/Models/WorkerProof.php
+++ app/Models/WorkerProof.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class WorkerProof extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['user_id', 'type', 'image_path'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
