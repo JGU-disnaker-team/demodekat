8c2ba60c28f99d10afdac91c487d296884630d80
hellokirana <intaniasyahla@gmail.com>
2025-02-23 11:14:32 +0700

    FIX - hapus meta

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -17,10 +17,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <meta name="initial-province" content="{{ Auth::user()->province_code }}">
-    <meta name="initial-city" content="{{ Auth::user()->city_code }}">
-    <meta name="initial-district" content="{{ Auth::user()->district_code }}">
-    <meta name="initial-village" content="{{ Auth::user()->village_code }}">
+
 
 </head>
 
